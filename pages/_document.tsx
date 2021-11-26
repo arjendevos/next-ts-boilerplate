b17c081cbb7d5bca14f30d0cab1ade4698783536
Arjen de Vos <arjendevoz@gmail.com>
2021-11-26 19:48:21 +0100

    update templates

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,14 +12,6 @@ class MyDocument extends Document {
       <Html>
         <Head />
         <meta key="0" name="viewport" content="width=device-width, initial-scale=1.0" />
-        {/* Load your fonts */
-        /* <link
-          rel="preload"
-          href="/fonts/grold/Grold-Regular.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        /> */}
         <body>
           <Main />
           <NextScript />
